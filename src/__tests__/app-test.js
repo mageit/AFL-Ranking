747982024b7d4fe83a89c758682fca746b22ee9d
Jack Zhong <jzho21@gmail.com>
2017-03-03 17:57:39 +1100

    Add unit test cases

--- src/__tests__/app-test.js
+++ src/__tests__/app-test.js
@@ -1,6 +1,41 @@
-require('../components/player');
-describe('Try to load player component', function () {
-  it('loads component without problems', function () {
+import TestUtils from 'react-addons-test-utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import assert from 'assert';
+import Player from '../components/player';
 
+describe('Try to load player component', () => {
+
+  let renderer: React.ShallowRenderer;
+
+  //Prepare prop data for testing purpose
+  const TEAM = {teamName: "AFL"};
+  const PLAYER = {
+    playerName: {
+      givenName: "John",
+      surname: "Smith"
+    }
+  };
+  const POSITION = "Midfielder";
+
+  const RATINGS = [
+    {
+      ranking: 1
+    }
+  ];
+
+  beforeEach(()=>{
+      renderer = TestUtils.createRenderer();
+      renderer.render(<Player
+            position={POSITION}
+            team={TEAM}
+            player={PLAYER}
+            detailedRatings={RATINGS}
+      />);
+  });
+
+  it('loads component and check it is a div', function () {
+      const result = renderer.getRenderOutput();
+      assert.equal(result.type, 'div');
   });
 });
